2a7804a8b620a8c3eab0f7b39a1e0ae53b5b6c0a
Amol Gupta <amolgupta1989@gmail.com>
2021-08-21 22:26:34 +0100

    compose dependencies

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -38,6 +38,18 @@ dependencies {
     implementation("io.ktor:ktor-client-android:1.5.4")
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.2.0")
 
+    implementation("androidx.compose.ui:ui:1.0.1")
+    // Tooling support (Previews, etc.)
+    implementation("androidx.compose.ui:ui-tooling:1.0.1")
+    // Foundation (Border, Background, Box, Image, Scroll, shapes, animations, etc.)
+    implementation("androidx.compose.foundation:foundation:1.0.1")
+    // Material Design
+    implementation("androidx.compose.material:material:1.0.1")
+    // Integration with activities
+    implementation("androidx.activity:activity-compose:1.3.1")
+
+
+
     api("com.google.firebase:firebase-core:19.0.1")
     api("com.google.firebase:firebase-analytics:19.0.1")
     api("com.google.firebase:firebase-perf-ktx:20.0.2")
--- build.gradle.kts
+++ build.gradle.kts
@@ -6,10 +6,10 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
-        classpath("com.android.tools.build:gradle:4.1.3")
-        classpath("org.jetbrains.kotlin:kotlin-serialization:1.5.10")
-        classpath("com.google.gms:google-services:4.3.8")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.20")
+        classpath("com.android.tools.build:gradle:7.0.1")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:1.5.20")
+        classpath("com.google.gms:google-services:4.3.10")
         classpath("com.google.firebase:perf-plugin:1.4.0")
         classpath("gradle.plugin.com.onesignal:onesignal-gradle-plugin:[0.12.10, 0.99.99]")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.5")
